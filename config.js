0c18797d26b0c8c751a1eec879d620a17c1001d0
loverss <a786b399@gmail.com>
2023-11-06 11:02:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.autoread = false // Auto Message read(true for on, false for off)
 global.alwaysonline = true // Always Online(true for on, false for off)
 global.statusread = true // Auto status read 🗿🖐🏻(true for on, false for off)
 global.private = true // worktype(true for private, false for public)
-global.autoreact = false // Auto react ( verum myra aah 🤣🙌🏻 )
+global.autoreact = true // Auto react ( verum myra aah 🤣🙌🏻 )
 global.autodlyt = false // Auto YouTube video downloader(true for on, false for off)
 global.largethumb = "true" // for large thumbnail 
 global.version = "1.5.0"
@@ -33,26 +33,26 @@ global.version = "1.5.0"
 // Required variables
 //---------------------------------
 global.prefix = "." // use "" for removing prefix
-global.ownername = "ᴛᴇᴀᴍ ᴇx-ʙᴏᴛ-ᴢ" // Bot Owner name
-global.botname = "x-ʙᴏᴛ-ᴍᴅ" //bot name
-global.thumb = "https://i.imgur.com/IIHEyLI.jpg" // bot image overall
-global.ownernumber = ["917012984396","917594898804","919961857267"] //SODO / OWNER NUMBER
-global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*_" // Caption 
-global.packname = `X-BOT-MD` //sticker pack
-global.author = "TEAM EX-BOT-Z 🗿" //sticker name
+global.ownername = "Sufyan Jan" // Bot Owner name
+global.botname = "sufyan-bot" //bot name
+global.thumb = "https://i.imgur.com/1N0OycJ.jpeg" // bot image overall
+global.ownernumber = ["923036971399","923036971399","923070496021"] //SODO / OWNER NUMBER
+global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Sufyan Jan*_" // Caption 
+global.packname = `sufyab-boT` //sticker pack
+global.author = "TEAM VOP 🗿" //sticker name
 //---------------------------------
 // Owner Mention Audio
 //---------------------------------
 global.ownermention = true // to turn off type false  || turn on type true 👌🏻
-global.mtitle = "X-Bot-Md" // mention tittle
-global.mbody = "TEAM EX BOT-Z" // mention discription 
-global.mthumb = 'https://i.imgur.com/J2x8YMw.jpg' // mention thumbnail 
-global.murl = 'https://instagram.com/aswin_sparky.exe'
+global.mtitle = "SUFYAN-JAN" // mention tittle
+global.mbody = "SUFYAN" // mention discription 
+global.mthumb = 'https://i.imgur.com/1N0OycJ.jpeg' // mention thumbnail 
+global.murl = 'https://instagram.com/Sufi-writer.s'
 global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
 //---------------------------------
 // All in One Url ( you can add any url you want )
 //---------------------------------
-global.url = 'https://chat.whatsapp.com/JjzEUDkJgke1vPGK3GAvG6' // mention url
+global.url = 'https://chat.whatsapp.com/CgqsGKXnEvxCRLnIboXpda' // mention url
 //---------------------------------
 // No need of editing ( Edit aaki oombanda 🗿🖐🏻) kuduthal edit aakiyal kolamm aayum myra 🖐🏻🤣
 //---------------------------------
